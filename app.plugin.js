2298e48b223d9f33dedbed66353b9b9f1917b848
Talha Salt <me@talha.io>
2026-03-23 11:50:45 +0300

    Enhance withFidbek to manage Maven repositories
    Refactor withFidbek function to integrate withProjectBuildGradle and handle Maven repository paths.

--- app.plugin.js
+++ app.plugin.js
@@ -1,5 +1,38 @@
-function withFidbek(config) {
-  return config;
-}
+ const path = require("path")
+  const {
+    createRunOncePlugin,
+    withProjectBuildGradle,
+  } = require("@expo/config-plugins")
+  const pkg = require("./package.json")
 
-module.exports = withFidbek;
+  function withFidbek(config) {
+    return withProjectBuildGradle(config, (config) => {
+      if (config.modResults.language !== "groovy") {
+        return config
+      }
+
+      const packageRoot = path.dirname(
+        require.resolve("@saltware/fidbek-react-native/package.json", {
+          paths: [config.modRequest.projectRoot],
+        }),
+      )
+
+      const mavenRepoPath = path
+        .relative(
+          config.modRequest.platformProjectRoot,
+          path.join(packageRoot, "android", "maven"),
+        )
+        .replace(/\\/g, "/")
+
+      if (!config.modResults.contents.includes(mavenRepoPath)) {
+        config.modResults.contents = config.modResults.contents.replace(
+          /(allprojects\s*\{\s*repositories\s*\{)/m,
+          `$1\n    maven { url = uri("${mavenRepoPath}") }`,
+        )
+      }
+
+      return config
+    })
+  }
+
+  module.exports = createRunOncePlugin(withFidbek, pkg.name, pkg.version)
